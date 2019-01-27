bda44263fd2c85e11c19981bf63b9cf99998aed6
Quentin Jensen <jensenq@wwu.edu>
2019-01-27 12:18:08 -0800

    only the first char in the input-string is considered

--- prog1/hangman.c
+++ prog1/hangman.c
@@ -12,6 +12,8 @@ void verify(char guess, char* secretword, char* progress, int* guesses);
 
 int main( int argc, char **argv) {
 	char* secretword;
+	char inputBuf[256];
+	char guess;
 
 	if (argc != 2)
 	{
@@ -28,25 +30,39 @@ int main( int argc, char **argv) {
 		progress[i+1] = '\0';
 	}
 	
-	char guess;
 
 	while(guesses>0){
 		printf("Board: %s (%d guesses left)\n", progress, guesses);
 		printf("Enter guess: ");
-		scanf(" %c", &guess);
+
+		if (fgets(inputBuf, sizeof inputBuf, stdin) == NULL) {
+      	printf("Exiting.\n");
+      	exit(1);
+		}
+		guess = inputBuf[0];
+
 		verify(guess, secretword, progress, &guesses);
+
 		if(!strcmp(secretword,progress)){
 			printf("You Win!\n");
+			printf("The word was: %s\n", secretword);
 			exit(0);
 		}
 	}
+
 
 	printf("You lose! cry about it, idiot.\n");
 	exit(0);
 }
 
+
+
+/* verify
+ *	checks if the guess is correct, and updates the progress string accordingly
+*/
 void verify(char guess, char* secretword, char* progress, int* guesses){
 	bool right = false;
+
 	for(int i=0; i<strlen(secretword);i++){
 		if(guess==secretword[i]){
 			progress[i] = guess;
