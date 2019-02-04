8fecf4d39663eeff7ad995b8151c94d56e89a916
Quentin Jensen <jensenq@wwu.edu>
2019-02-03 17:38:45 -0800

    add hangman to server

--- prog1/prog1_server.c
+++ prog1/prog1_server.c
@@ -8,6 +8,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+void verifyAndUpdate(char guess, char* secretword, char* progress, int* guesses);
+int startGame(char* secretword); 
 
 #define QLEN 6 
 int visits = 0; 
@@ -23,10 +27,12 @@ int main(int argc, char **argv) {
 	int optval = 1; 
 	char buf[1000]; 
 
-	if( argc != 2 ) {
+	char* secretword = argv[2];
+
+	if( argc != 3 ) {
 		fprintf(stderr,"Error: Wrong number of arguments\n");
 		fprintf(stderr,"usage:\n");
-		fprintf(stderr,"./server server_port\n");
+		fprintf(stderr,"./server server_port secretword\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -68,17 +74,104 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-/*================== END CONNECTION INIT ========================*/
+/*================== END CONNECTION-INIT ========================*/
 
-		while (1) {
+	while (1) {
 		alen = sizeof(cad);
 		if ( (sd2=accept(sd, (struct sockaddr *)&cad, &alen)) < 0) {
 			fprintf(stderr, "Error: Accept failed\n");
 			exit(EXIT_FAILURE);
 		}
 		
-		send(sd2,buf,strlen(buf),0);
-		close(sd2);
+		const pid_t cpid = fork();
+		switch(cpid) {
+		
+			//error
+			case -1: {
+		  		perror("fork");
+		  		break;
+			}
+		
+			//child
+			case 0: {
+				close(sd);
+				startGame(secretword);
+		  		exit(0);
+		 		break;
+			}
+		
+			//parent
+			default: {
+				close(sd2);
+		  		break;
+			}
+		}
+	}
+
+//send(sd2,buf,strlen(buf),0);
+
+}//end main
+
+
+
+/*	startGame
+ * -server-side logic of the game
+ * -server can run multiple instances of this game for multiple clients
+ *
+ *	secretWord: user-input string that the client tries to guess
+*/
+int startGame(char* secretword) {
+	char inputBuf[256];
+	int guesses = strlen(secretword);
+	char* progress = malloc(sizeof(char) * guesses);
+
+	for(int i=0; i<strlen(secretword); i++){
+		progress[i] = '_';
+		progress[i+1] = '\0';
+	}
+
+/*
+	while(guesses>0){
+
+		send guesses and progress
+
+		await a guess 
+
+		verifyAndUpdate(guess, secretword, progress, &guesses);
+
+		if(!strcmp(secretword,progress)){
+			send uint8_t 255
+		}
+	}
+	
+	send uint8_t 0
+*/
+}
+
+
+
+/* verify
+ *	-checks if guess is in secretword, and updates progress 
+ *	 and guesses accordingly
+ * 
+ * guess: client-input character
+ * secretword: server-input string
+ * progress: the "board" of underscores and letters
+ * guesses: number of guesses a player has left.
+*/
+void verifyAndUpdate(char guess, char* secretword, char* progress, int* guesses){
+	bool correct = false;
+
+	for(int i=0; i<strlen(secretword);i++){
+		if(guess==secretword[i]){
+			progress[i] = guess;
+			correct = true;
+		}
+	}
+
+	if(!correct){
+//		printf("wrong\n");
+		(*guesses)--;
 	}
 }
 
